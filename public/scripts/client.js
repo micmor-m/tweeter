10378e90d6409da85b27e67c96203852043e25b9
micmor-m <micmor.m@gmail.com>
2020-08-12 20:00:03 +0000

    implement dinamic tweeter adding

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,95 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//Test / driver code (temporary). Eventually will get this from the server.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1261116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+
+
+ const createTweetElement = function(data) {
+  console.log("DATA IS INSIDE");
+  console.log(data)
+
+  let datetime = new Date(data.created_at);
+  let today = Date.now();
+
+  //(date2 - date1) / 1000 / 60 / 60 / 24
+  let diffDays = Math.floor((today - datetime) /1000 / 60 / 60 / 24);
+  console.log(diffDays);
+  
+//const item = `
+  const tweet = `
+  <article id="tweet" class="tweet_header"> 
+    <div class = msm_top>
+      <div id="name_user">
+        <img src="${data.user.avatars}"> 
+        <label  for="tweet-user">"${data.user.name}"</label>
+      </div>
+      <label for="tweet-sender">"${data.user.handle}"</label>
+    </div>
+    <textarea name="text" id="tweet-text">"${data.content.text}"</textarea>
+    <div id="days">
+      <p id="days_ago" type="submit">"${diffDays} days ago"</p>
+      <div id="icons">
+        <button id="btn1" type="submit"><img src="/images/flag.png"> </button>
+        <button id="btn2" type="submit"><img src="/images/arrow.png"> </button>
+        <button id="btn3" type="submit"><img src="/images/heart.png"></button>
+      </div>        
+    </div>
+    </article>
+    `
+
+  return tweet;
+
+}
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  for (let tweet of tweets) {
+    let tweetToAdd = createTweetElement(tweet);
+    console.log("Tweet to add ", tweetToAdd);
+   $('#tweeter').append(tweetToAdd);
+   //$('#tweet').append(tweetToAdd);
+  }
+}
+
+
+//const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+//console.log($tweet); // to see what it looks like
+//$('.container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+//const $tweet_header = $(`<article class="tweet_header">Hello world</article>`);
+
+$(document).ready(function() {
+  
+renderTweets(data);
+
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,24 @@
+//all event to be done inside this event handler because we need to wait the page is fully loaded
+//before request any information in it, otherwise will get an error
 $(document).ready(function() {
-  // --- our code goes here ---
-  console.log("Doc is ready")
+  
+  console.log("Doc is ready");
 
-//console log when click on button
-$("#btn").on('click', function() {
-  console.log(this); //The this keyword is a reference to the button
-});
+  //console log when click on button
+  $("#btn").on('click', function() {
+    //evt.preventDefault();
+    //The this keyword is a reference to the button
+    console.log(this);
+  });
 
-//console.log when press the key in the input field
-//keydown to work properly
-$("#tweet-text").on('keydown', function() {
-  console.log(this.value); //The this keyword is a reference to the button
-  console.log("tEXT LENGTH IS :", this.value.length)
-  //let templateVars = {counter: this.value.length };
-  //res.render("index.html", templateVars);
-  $(".counter").text(140 - this.value.length);
+  //console.log when press the key in the input field
+  //keydown to work properly
+  $("#tweet-text").on('keydown', function() {
+    //evt.preventDefault();
+    console.log(this.value);
+    console.log("tEXT LENGTH IS :", this.value.length);
+  
+    $(".counter").text(140 - this.value.length);
 
     if ((140 - this.value.length) < 0) {
       console.log( $(".counter"))
@@ -24,7 +28,7 @@ $("#tweet-text").on('keydown', function() {
       //in not remove the class and come back the black color
       $(".counter").removeClass("negativeNumber");
     }
-});
+  });
 
 //This is a right way to do it in JS but it can be done 
 //more simply directly in css file
